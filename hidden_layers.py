fe302d279f368bd1f1793baf4c11d7d83ea93cd3
Paul Saikko <paul.saikko@cs.helsinki.fi>
2017-12-29 00:42:54 +0200

    add some visualization for hidden layer weights

--- hidden_layers.py
+++ hidden_layers.py
@@ -48,21 +48,21 @@ flattened_train_x = (numpy.reshape(train_x, (N_train, img_size)) / 256)
 onehot_test_y = to_onehot(test_y)
 onehot_train_y = to_onehot(train_y)
 
-layer_sizes = [img_size, 10, n_cats]
+layer_sizes = [img_size, 12, 11, n_cats]
 
 # initialize random weights with mean 0
 W = [2.0 * numpy.random.rand(layer_sizes[i+1], layer_sizes[i]) - 1.0
      for i in range(len(layer_sizes) - 1) ]
 
 # iterations of gradient descent
-epochs = 500
+epochs = 1
 
 # gradient descent step size
 eps = 0.0001
 
-pyplot.ion()
-pyplot.figure()
-pyplot.axis([0,epochs,0,1])
+#pyplot.ion()
+pyplot.figure(1)
+#pyplot.axis([0,epochs,0,1])
 
 losses = []
 accs = []
@@ -71,8 +71,8 @@ for i in range(epochs):
 	deltas = []
 
 	# compute output of each layer
-	H = forward_propagate(flattened_train_x, W)
-	pred_y = H[-1]
+	layer_outputs = forward_propagate(flattened_train_x, W)
+	pred_y = layer_outputs[-1]
 
 	# count correct predictions
 	result = correct(onehot_train_y, pred_y)
@@ -92,12 +92,13 @@ for i in range(epochs):
 	err = pred_y - onehot_train_y
 
 	# backpropagation
-	for (w,h,out) in zip(reversed(W), list(reversed(H[:-1]))+[flattened_train_x], reversed(H)):
+	layer_inputs = [flattened_train_x] + layer_outputs[:-1]
+	for (w,layer_input,layer_output) in reversed(list(zip(W, layer_inputs, layer_outputs))):
 		# error on previous layer * derivative of activation (sigmoid)
-		gradient = err * out * (1.0 - out)
+		gradient = err * layer_output * (1.0 - layer_output)
 
 		# apply gradient with step size to next layer output
-		delta = -eps * numpy.dot(gradient.T, h)
+		delta = -eps * numpy.dot(gradient.T, layer_input)
 
 		# update weights after backpropagation finished
 		deltas += [delta]
@@ -117,5 +118,34 @@ num_correct = sum(correct_predictions)
 print(num_correct)
 print("%d%% correct" % (num_correct * 100.0 / N_test))
 
-# keep plot visible
+
+vis_weights = [[W[0][i] for i in range(W[0].shape[0])]]
+
+
+for (layer_size, layer_index) in zip(layer_sizes[2:], range(2,len(layer_sizes))):
+	layer_weights = []
+
+	for i in range(layer_size):
+		node_w = W[layer_index-1][i]
+
+		prev_wgts = []
+		for j in range(len(node_w)):
+			prev_wgts += [node_w[j] * vis_weights[-1][j]]
+
+		prev_wgts = sum(prev_wgts) / len(node_w)
+		layer_weights += [prev_wgts]
+
+	vis_weights += [layer_weights]
+
+H = len(vis_weights)
+W = max(layer_sizes[1:])
+
+pyplot.figure(2)
+for layer in range(H):
+	for weights in range(len(vis_weights[layer])):
+		pyplot.subplot(H, W, 1 + layer*W + weights)
+		pyplot.imshow(numpy.reshape(vis_weights[layer][weights], (img_height, img_width)))
+		pyplot.axis('off')
+
+# keep plots visible
 pyplot.show()
